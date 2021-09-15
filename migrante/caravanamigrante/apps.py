b4d5d7d546cbb01cdca6366c9e836bda045c5527
Jose Gutierrez <antvas745@gmail.com>
2021-09-14 22:27:43 -0600

    Se creo el modulo caravana migrante

---
+++ migrante/caravanamigrante/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CaravanamigranteConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'caravanamigrante'
--- migrante/migrante/settings.py
+++ migrante/migrante/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'usuarios',
     'donacion',
     'bitacora',
+    'caravanamigrante',
 ]
 
 MIDDLEWARE = [
